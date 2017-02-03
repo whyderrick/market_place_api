4a39646750dc91579e2a59335ee83e1d254e4bc2
Derrick Carr <derrick.the.future@gmail.com>
2017-02-02 23:04:19 -0800

    Add API v1 versioning scope.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ end
 MarketPlaceApi::Application.routes.draw do
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
 
+    end
   end
 end
