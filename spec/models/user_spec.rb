ec2b39745aa1976e5b5c689ec00f6de9720b542c
Derrick Carr <derrick.the.future@gmail.com>
2017-02-07 23:35:29 -0800

    Add Initial Spec for User Model
    This commit adds test to ensure that the user model is responding
    to its attributes. It creates a user factory in FactoryGirl and
    uses an explicit subject at the top of the test's describe block and
    the `is_expected.to` method in RSpec, which minimizes some of the typing
    around tests for simple conditions.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){FactoryGirl.build(:user)}
+  subject { user }
+  context "Responds to attributes" do
+    it { is_expected.to respond_to( :email ) }
+    it { is_expected.to respond_to( :password ) }
+    it { is_expected.to respond_to( :password_confirmation ) }
+  end
+
+  it { is_expected.to be_valid }
 end
